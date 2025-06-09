87b5c9960add63d0543ee70565a88d3ce42c1fab
Jose David Anacona <jdanaconapira@hotmail.com>
2025-06-09 14:19:04 -0500

    Taller 2 Reto 3 ajuste cobertura Nueva rama v2

--- app/falloParaBandit.py
+++ app/falloParaBandit.py
@@ -1,3 +1,6 @@
 def uso_eval(dato):
     # Esto genera una alerta de seguridad (B307) en Bandit
     return eval(dato)
+
+def run_this_dangerous_code():
+    return "Este código tiene fallas de seguridad"
